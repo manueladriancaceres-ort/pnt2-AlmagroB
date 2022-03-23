8ac1511302d2f7aa1bf8db6920767c96263f7cf6
AlexTeper99 <ateper@fi.uba.ar>
2022-03-22 22:29:19 -0300

    Ejercicio terminado y corregido por el profesor

--- Grupo_3/Clase2/index.js
+++ Grupo_3/Clase2/index.js
@@ -7,10 +7,10 @@ const proceso = {
     getCodigo: function() {
         return this.codigo;
     },
-    stockAcutal: [{codigoProducto: 1, cantidad: 10, stockMinimo: 5},
+    stockAcutal: [{codigoProducto: 1, cantidad: 11, stockMinimo: 5},
         {codigoProducto: 2, cantidad: 8, stockMinimo: 12},
-        {codigoProducto: 3, cantidad: 8, stockMinimo: 13},
-        {codigoProducto: 4, cantidad: 10, stockMinimo: 5}],
+        {codigoProducto: 3, cantidad: 9, stockMinimo: 13},
+        {codigoProducto: 4, cantidad: 8, stockMinimo: 5}],
 
     stockNuevo: [{codigoProducto: 3, paraAgregar: 3},
         {codigoProducto: 2, paraAgregar: 10}],
@@ -19,26 +19,29 @@ const proceso = {
     calcularMinimos: function() {
 
         /*
-        //consigna 1: Encontrar el producto con codigo minimo
-        let productosStockMinimo = []; //PREGUNTA: PODRIA HACER CONST Y EN EL NUEVO MINIMO UN .CLEAR ?
-        let minimo = proceso.stockAcutal[0].stockMinimo;
+        //consigna 1: Encontrar el producto con cantidad minima
+        let productosStockMinimo = [];
+        let minimo = 100000;
         for (let index =0 ; index< this.stockAcutal.length; index++){
-          if(this.stockAcutal[index].stockMinimo < minimo) {
-              minimo = this.stockAcutal[index].stockMinimo;
-              productosStockMinimo = this.stockAcutal[index];
-          }else if(this.stockAcutal[index].stockMinimo == minimo){
+          if(this.stockAcutal[index].cantidad < minimo) {
+              minimo = this.stockAcutal[index].cantidad;
+              productosStockMinimo = [];
+              productosStockMinimo.push(this.stockAcutal[index]);
+
+          }else if(this.stockAcutal[index].cantidad === minimo){
               productosStockMinimo.push(this.stockAcutal[index]);
           }
-        }*/
+        }
+*/
 
+    //consigna 2: Encontrar los productos cuya cantidad es menor a su atributo stockMinimo
         const productosStockMinimo = []
-        //consigna 2: Encontrar los productos cuya cantidad es menor a su atributo stockMinimo
         for (let i = 0; i < this.stockAcutal.length; i++) {
             if(this.stockAcutal[i].cantidad < this.stockAcutal[i].stockMinimo){
                 productosStockMinimo.push(this.stockAcutal[i])
             }
-
         }
+
         return productosStockMinimo;
     } ,
 
@@ -47,13 +50,14 @@ const proceso = {
         let j = 0;
         let loEncontre = false;
         for (let i = 0; i < this.stockNuevo.length; i++) {
-            //Si el stock actual esta ordenado y el cod producto empieza en 1
-           // this.stockAcutal[this.stockNuevo[i].codigoProducto - 1].cantidad += this.stockNuevo[i].paraAgregar;
+            //Opcion A: Si el stock actual esta ordenado y el cod producto empieza en 1
+         //   this.stockAcutal[this.stockNuevo[i].codigoProducto - 1].cantidad += this.stockNuevo[i].paraAgregar;
 
+            //Opcion real.
             j=0;
             loEncontre = false;
             while(j< this.stockAcutal.length && !loEncontre){
-                if(this.stockAcutal[j].codigoProducto == this.stockNuevo[i].codigoProducto){
+                if(this.stockAcutal[j].codigoProducto === this.stockNuevo[i].codigoProducto){
                     this.stockAcutal[j].cantidad += this.stockNuevo[i].paraAgregar
                     loEncontre = true;
                 }
