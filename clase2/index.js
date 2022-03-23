8f6ac141df64e13e7a3ada06e398da6e6fbf89bd
fmothe <mothevv.13@gmail.com>
2022-03-22 22:11:46 -0300

    Ejercicio

--- clase2/index.js
+++ clase2/index.js
@@ -1,34 +1,11 @@
-console.log("Grupo ...");
-
-// var ya no se usa... se usa let
-// constantes
-const MI_VARIABLE = 45;
-console.log(MI_VARIABLE);
-// ERROR -> MI_VARIABLE = 10;
-
-const unVector = [];
-unVector.push(34);
-unVector.push(true);
-unVector.push("algun texto");
-console.log(unVector);
-console.log(typeof unVector);
-
-const otroVector = unVector;
-otroVector[0] = "Principio";
-
-console.log(unVector);
-console.log(otroVector);
-
-
-// esto da error en tiempo de ejecucion: let unVector = 30
-
+//Ejercicio 1 clase 2
 const proceso = {
     codigo: 1,
     descripcion: "Proceso stock",
     getCodigo: function() {
         return this.codigo;
     },
-    stockAcutal: [{codigoProducto: 1, cantidad: 10, stockMinimo: 5},
+    stockActual: [{codigoProducto: 1, cantidad: 10, stockMinimo: 5},
                   {codigoProducto: 2, cantidad: 8, stockMinimo: 12},
                   {codigoProducto: 3, cantidad: 8, stockMinimo: 13}],
     stockNuevo: [{codigoProducto: 3, paraAgregar: 3},
@@ -37,27 +14,30 @@ const proceso = {
     // Recorrer stockAcual y devolver los productos con stock minimo
     calcularMinimos: function() {
         const productosStockMinimo = [];
+        for (let index = 0; index < this.stockActual.length; index++) {
+            const element = this.stockActual[index];
+            if(element.cantidad <= element.stockMinimo) {
+                productosStockMinimo.push(element);
+            }
+        }
         return productosStockMinimo;
     } ,   
 
     // Actualizar stockActual con stockNuevo
     actualizarStock: function() {
-
+        for (let index = 0; index < this.stockNuevo.length; index++) {
+            const element = this.stockNuevo[index];
+            for (let index2 = 0; index2 < this.stockActual.length; index2++) {
+                const element2 = this.stockActual[index2];
+                if(element.codigoProducto === element2.codigoProducto) {
+                    element2.cantidad += element.paraAgregar;
+                    console.log(`Se agrego ${element.paraAgregar} unidades al producto ${element2.codigoProducto}`);
+                }
+            }
+        }
     }
 
 }
 
 console.log(proceso.calcularMinimos());
-// proceso = 45;
-// console.log(proceso);
-
-// para evitar un cambio del estilo let proceso = 5, por eso se usa const
-
-// Hagamos la prueba
-unArreglo = [1, 2, 3, 4, 5];
-let suma = 0;
-for (let index = 0; index < unArreglo.length; index ++){
-    const element = unArreglo[index];
-    suma += element;
-}
-console.log(suma);
+console.log(proceso.actualizarStock())
