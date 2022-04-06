151bebf458941940145ea26136e32de0d585ccfa
AlexTeper99 <ateper@fi.uba.ar>
2022-04-05 22:18:28 -0300

    Listas en Vue

--- Grupo_3/clase 4/listas.js
+++ Grupo_3/clase 4/listas.js
@@ -1,7 +1,20 @@
 const objVue = Vue.createApp({
     data() {
         return {
-            message: 'Holaaaa Vue!'
+            gestiones: [
+                {id: 13,desc: "Llamado 1",prioridad:200},
+                {id: 12,desc: "Llamado 2",prioridad:203},
+            ],
+            gestion: {id: 0, desc: " ", prioridad: 0}
+        }
+    },
+    methods: {
+        agregar(){
+            this.gestiones.push({id: this.gestion.id, desc: this.gestion.desc,prioridad: this.gestion.prioridad});
+
+        },
+        ordenarPorPrioridad(){
+            this.gestiones.sort((a,b) => a.prioridad - b.prioridad)
         }
     }
 }).mount('#app')
