e19510d0f829be265cfcbc57355d3153e902288f
manueladriancaceres-ort <manuel.caceres@ort.edu.ar>
2022-05-10 21:13:55 -0300

    services2

--- clase10/ejercicio/backend/src/index.js
+++ clase10/ejercicio/backend/src/index.js
@@ -1,10 +1,20 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
 const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 const port = 3001
 
-// simulo una base de datos
+// simulo una base de datos en memoria
+// vendecores
+const lista = [{codigo:100,nombre:"Maria"},{codigo:101,nombre:"Juan"}];
 
-
+app.get('/api/lista', (req,res) => {
+  // CONSULTA A BASE DE DATOS
+  res.json(lista);
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
